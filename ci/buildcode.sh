a88835ccfebdc5a9e467c38e543cdc44d59e4e20
Sjoerd Adema <sjoerd@kristalsoftware.nl>
2018-04-19 22:47:46 +0200

    tmp no build

--- ci/buildcode.sh
+++ ci/buildcode.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-cd resource-concourse-echo
-mvn clean package -DskipTests
-cp -a target/* ../artifacts/
+#cd resource-concourse-echo
+#mvn clean package -DskipTests
+#cp -a target/* ../artifacts/
 
-
+touch artifacts/blabla.jar
