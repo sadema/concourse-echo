21e59bf1b62c3a0d1f761e6d7f46a5f5a7c10de4
Sjoerd Adema <sjoerd@kristalsoftware.nl>
2018-04-19 23:59:05 +0200

    pipeline variables

--- ci/buildcode.sh
+++ ci/buildcode.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-#cd resource-concourse-echo
-#mvn clean package -DskipTests
-#cp -a target/* ../artifacts/
-
-touch artifacts/blabla.jar
+cd resource-concourse-echo
+mvn clean package -DskipTests
+cp -a target/* ../artifacts/
